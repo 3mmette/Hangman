35958a84bab19b8497e7fc441c627dd4c35b6bc9
Emmette <emmette.taylor@hotmail.com>
2022-05-01 12:19:42 +0800

    Version 1.4

---
+++ gameplay.py
@@ -0,0 +1,207 @@
+import random
+import time
+
+from utility_functions import *
+from text_art import *
+
+
+qwerty_keys = [
+    f"{'Q':>2} {'W':>5} {'E':>5} {'R':>5} {'T':>5} {'Y':>5} {'U':>5} {'I':>5} {'O':>5} {'P':>5}",
+    f"{'A':>5.5} {'S':>5} {'D':>5} {'F':>5} {'G':>5} {'H':>5} {'J':>5} {'K':>5} {'L':>5}",
+    f"{'Z':>8.5} {'X':>5} {'C':>5} {'V':>5} {'B':>5} {'N':>5} {'M':>5}"
+]
+
+correct_quotes = ["your one step closer to freedom!",
+                  "was that luck or skill?",
+                  "great guess, another letter in!"
+                  ]
+
+incorrect_quotes = ["your better then that!",
+                    "do you want to die?",
+                    "why would you guess that?",
+                    "that was stupid of you!"
+                    ]
+
+
+def gameplay_box(height, width, user_name, comment, picture, progress, lives, keyboard):
+    """
+    Prints a box using the following arguments to provide information to the user
+    Different comments come from the lists, located at the top of this file, depend on the if the guess was good or not
+    :param height: height of the box
+    :param width: width of the box
+    :param user_name: name of the user
+    :param comment: a varying text, commenting on how the game is going
+    :param picture: a visual representation of the remaining lives
+    :param progress: the word the user is trying to guess, '_' for any letters yet to be guessed
+    :param lives: the number of incorrect guesses the user has to complete the game
+    :param keyboard: representation of the qwerty keyboard for the game
+    """
+    clear_screen()
+    inner_width = width - 4
+    line_count = 2
+    line_count += solid_line(width)
+    line_count += header(inner_width, hangman)
+    line_count += solid_line(width)
+    line_count += text_line(inner_width, f"{user_name}, {comment}", "^")
+    line_count += solid_line(width)
+    line_count += text_line(inner_width, picture, "^")
+    line_count += empty_line(inner_width)
+    line_count += text_line(inner_width, f"Lives Remaining: {lives}", "^")
+    line_count += solid_line(width)
+    line_count += text_line(inner_width, "Word Progress", "^")
+    line_count += text_line(inner_width, progress, "^")
+    line_count += solid_line(width)
+    line_count += text_line(inner_width, "Available Letters", "^")
+    line_count += text_line(inner_width, keyboard)
+    line_count += solid_line(width)
+    for extra in range((height - line_count)):
+        empty_line(inner_width)
+    text_line(inner_width, "To quit, simply guess 0 (Zero)", "^")
+    line_count += solid_line(width)
+
+
+def validate_character(height, width, user_name, picture, lives, progress, guesses, keyboard):
+    while True:
+        guess = input("Guess: ").upper()
+
+        # if the user wants to quit.
+        if guess == "0":
+            break
+
+        # If the user enters a single character.
+        if len(guess) == 1:
+
+            # If the guess is valid.
+            if guess in str(keyboard):
+                break
+
+            # If they have already guessed that letter.
+            elif guess in guesses:
+                gameplay_box(height, width, user_name, "choose from available letters below!",
+                             picture[lives + 2], progress, lives, keyboard)
+                print("You have already guessed that letter!")
+
+            # If the guess is invalid
+            else:
+                gameplay_box(height, width, user_name, "that isn't a letter!", picture[lives + 2], progress,
+                             lives, keyboard)
+                print("Invalid character")
+
+        # If the user entered more than one character
+        else:
+            gameplay_box(height, width, user_name, "enter a single letter this time!", picture[lives + 2],
+                         progress, lives, keyboard)
+            print("You must enter a single character")
+    return guess
+
+
+def gameplay(height, width, user_name, word, difficulty, lives):
+    """
+    A combination of all the required functions to complete the version 1.4 enhancement
+    :param height: height of the box
+    :param width: width of the box
+    :param user_name: name of the user
+    :param word: the word the user is trying to guess
+    :param difficulty: the difficulty setting, used to select the respective text art list
+    :param lives: the number of incorrect guesses the user has to complete the game
+    """
+    # Overall result of the game.
+    result = None
+    # Result of the round, used to create comments on the game
+    round_result = None
+    # The respective text art list for displaying visual of lives.
+    picture = None
+    # The keyboard the game will use.
+    keyboard = qwerty_keys
+
+    # A string of all the letter guessed so far.
+    guesses = ""
+
+    # Uses the difficulty setting to select the respective text art list.
+    if difficulty == "Easy":
+        picture = easy
+    if difficulty == "Normal":
+        picture = normal
+    if difficulty == "Hard":
+        picture = hard
+
+    # Start time of the game
+    start = time.time()
+
+    # Creates a loop
+    while result is None:
+        failed = 0
+        progress = ""
+        # **Modified Original Code** Creates a word with any characters not guessed replaced with an underscore.
+        for character in word:
+            if character in guesses:
+                progress += " " + character + " "
+            else:
+                progress += " _ "
+                failed += 1
+
+        # If failed is equal to zero, the game is won so result is set to True and break stops the loop.
+        if failed == 0:
+            result = True
+            gameplay_box(height, width, user_name, "the word has be revealed. You win.", picture[lives + 2],
+                         progress, lives, keyboard)
+            break
+
+        # If it is the first round, tells the player time has started, otherwise comments on their progress.
+        if round_result is None:
+            comment = "your time starts now!"
+        elif round_result is True:
+            comment = random.choice(correct_quotes)
+        elif round_result is False:
+            comment = random.choice(incorrect_quotes)
+        else:
+            continue
+
+        # Create the gameplay box.
+        gameplay_box(height, width, user_name, comment, picture[lives + 2], progress, lives, keyboard)
+
+        # Get and validate the guess. Needs so many arguments to call the gameplay box within it.
+        guess = validate_character(height, width, user_name, picture, lives, progress, guesses, keyboard)
+
+        # Quits the game if 0 (Zero) is entered.
+        if guess == "0":
+            gameplay_box(height, width, user_name, "quitting won't save you. You lose anyway.", picture[2],
+                         progress, 0, keyboard)
+            result = None
+            break
+
+        # Add the guess to the list of characters used so far.
+        guesses += guess
+
+        # A temporary keyboard made to allow the replace function to work on list items. Makes guessed letter blank.
+        temp = list()
+        for line in keyboard:
+            if guess in line:
+                temp.append(line.replace(guess, " "))
+            else:
+                temp.append(line)
+
+        # New keyboard with guessed letter replaced.
+        keyboard = temp
+
+        # Sets round result variable for customizable text responses.
+        if guess not in word:
+            # lives counter decreases by 1
+            lives -= 1
+            round_result = False
+
+        if guess in word:
+            round_result = True
+
+        # If the lives are equal to zero, result set to False and loop breaks
+        if lives == 0:
+            gameplay_box(height, width, user_name, "no more chances remain. You lose.", picture[lives + 2],
+                         progress, lives, keyboard)
+            result = False
+            break
+
+    # Stops time for the game, then calculates time taken to reach this point.
+    stop = time.time()
+    time_taken = int(stop) - int(start)
+    input("[ENTER] to continue: ")
+    return result, time_taken
--- main.py
+++ main.py
@@ -9,124 +9,34 @@
 #
 #########################################################################
 
-import time
-from utility_functions import *
+# Import individual game sections into the main program
 from welcome_screens import welcome
 from word_selection import word_selection
 from difficulty_selection import difficulty_selection
+from gameplay import gameplay
 
 #########################################################################
-# CONSTANTS
-ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-MAX_LIVES = 10
-SCORE_AMOUNT = 10
+# CONSTANTS
 HEIGHT = 29
 WIDTH = 61
-
-
-##########################################################################
-# INPUT FUNCTIONS
-
-def askUserForSingleCharacter(options=[], prompt="Enter a character"):
-    choice = ""
-    if len(options) == 0:
-        options = ALPHABET
-    optionsList = ",".join(options)
-    while options.count(choice) <= 0:
-        print("Options are: " + optionsList)
-        choice = input(prompt + ": ")
-        if options.count(choice) <= 0:
-            print("OOPS! You made an error...")
-        # end if
-    # end while
-    return choice
-
 
 #########################################################################
 # THE MAIN PROGRAM
 #########################################################################
+
 if __name__ == "__main__":
-    # Welcome screens and get the name of the user.
+    # Welcome screens and input the name of the user.
     user_name = welcome(HEIGHT, WIDTH)
 
-    # Selects a random secret word.
+    # Selects a random secret word. Returns the word and the list it came from.
     word_selection_information = word_selection(HEIGHT, WIDTH, user_name)
 
-    # Select a difficulty
+    # Select a difficulty. Returns the number of lives and difficulty.
     difficulty_selection_information = difficulty_selection(HEIGHT, WIDTH, user_name)
 
+    # The game itself. Returns the result and time taken to complete the game.
+    gameplay_information = gameplay(HEIGHT, WIDTH, user_name, word_selection_information[0],
+                                    difficulty_selection_information[1], difficulty_selection_information[0])
 
-    print("Start guessing...")
-    time.sleep(0.5)
-    solid_line(WIDTH)
-
-    word = word_selection_information[0]
-
-    # create a guesses variable with an empty value
-    guesses = ''
-
-    # create a variable for the score
-    score = 0
-
-    # determine the number of lives
-    lives = MAX_LIVES
-
-    # list of letters not used
-    letters = ALPHABET
-
-    # Create a while loop
-    # check if the lives are more than zero
-    while lives > 0:
-        # make a counter that starts with zero
-        failed = 0
-
-        print("\nGuess the word: ", end="")
-        # for every character in the secret word
-        for char in word:
-            # see if the character is in the players guesses
-            if char in guesses:
-                # print then out the character
-                print(char, end="")
-            else:
-                # if not found, print a dash
-                print("-", end="")
-                # and increase the failed counter by one
-                failed += 1
-        # if failed is equal to zero
-        # print You Won
-        if failed == 0:
-            print("\nYou won")
-            # exit the script
-            break
-
-        print("\n\n")
-        # ask the user to guess a character
-        guess = askUserForSingleCharacter(letters, "Enter a character")
-        # add the guess to the list of characters used so far...
-        guesses += guess
-
-        # remove the guess from the list of available letters
-        letters.remove(guess)
-
-        # if the guess is not found in the secret word
-        if guess not in word:
-            # lives counter decreases by 1
-            lives -= 1
-            # print wrong
-            print("Guessed Wrong!\n")
-        else:
-            # increase the player score
-            score = score + SCORE_AMOUNT
-
-        # how many lives are left
-        print("You have", + lives, 'more guesses\n')
-
-        # if the lives are equal to zero
-        if lives == 0:
-            # print "You Lose"
-            print("You Lose")
-
-    # Press enter to quit
     finish = input("Press enter to finish. Goodbye " + user_name)
